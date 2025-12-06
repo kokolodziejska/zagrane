364c6e0451702da705bef5989967b0f7b04cb9fc
nela <kknela44@gmail.com>
2025-12-06 17:51:16 +0100

    add user

--- frontend/src/components/admin/menu pages/MangedUsers.tsx
+++ frontend/src/components/admin/menu pages/MangedUsers.tsx
@@ -6,12 +6,41 @@ import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 
 function MangeUser() {
+  const validationSchema = yup.object({
+    userName: yup
+      .string()
+      .trim()
+      .required('Podaj nazwę użytkownika')
+      .min(3, 'Nazwa użytkownika musi mieć przynajmniej 3 znaki')
+      .max(40, 'Nazwa użytkownika może mieć maksymalnie 40 znaków'),
+    name: yup
+      .string()
+      .trim()
+      .required('Podaj imię użytkownika')
+      .min(3, 'Imię użytkownika musi mieć przynajmniej 3 znaki')
+      .max(40, 'Imię użytkownika może mieć maksymalnie 15 znaków'),
+    surname: yup
+      .string()
+      .trim()
+      .required('Podaj nazwisko użytkownika')
+      .min(3, 'Nazwisko użytkownika musi mieć przynajmniej 3 znaki')
+      .max(40, 'Nazwisko użytkownika może mieć maksymalnie 15 znaków'),
+    department_id: yup.string().trim().required('Podaj dział'),
+    user_type_id: yup.string().trim().required('Podaj typ'),
+  });
+
   return (
     <div>
-      <div className="container flex flex-col items-center w-[80vw] gap-[2vh]">
+      <div
+        className="container flex flex-col items-center w-[80vw] h-[42vh] gap-[2vh] border-2 rounded-lg p-6"
+        style={{
+          borderColor: 'var(--primary)',
+        }}
+      >
         <h2 className="text-xl font-bold"> Dodaj nowego użytkonika</h2>
         <Formik
           enableReinitialize
+          validationSchema={validationSchema}
           initialValues={{
             userName: '',
             name: '',
@@ -26,7 +55,7 @@ function MangeUser() {
           {({ resetForm }) => (
             <div className="container flex flex-col items-center w-[80vw] gap-[2vh]">
               <Form>
-                <div className="grid grid-cols-3 grid-rows-3 gap-[1vw] h-full w-[65vw]">
+                <div className="grid grid-cols-3 grid-rows-2 gap-[1vw] h-[25vh] w-[65vw]">
                   <div className="grid gap-3 ">
                     <Field name="userName">
                       {({ field, meta }: any) => (
@@ -131,7 +160,7 @@ function MangeUser() {
                     <Field name="user_type_id">
                       {({ field, meta }: any) => (
                         <div className="grid gap-2">
-                          <Label htmlFor="user_type_id">Ttyp użytkownika</Label>
+                          <Label htmlFor="user_type_id">Typ użytkownika</Label>
                           <Input
                             id="user_type_id"
                             type="string"
@@ -152,6 +181,18 @@ function MangeUser() {
                       )}
                     </Field>
                   </div>
+                </div>
+                <div className="pt-2">
+                  <Button type="submit" className="w-full">
+                    Dodaj użytkonika
+                  </Button>
+                  {/* <p
+                    className={`text-xs text-center ${objectError ? 'text-red-500' : 'invisible'}`}
+                    aria-live="polite"
+                    id="submit"
+                  >
+                    {objectError || 'placeholder'}
+                  </p> */}
                 </div>
               </Form>
             </div>
