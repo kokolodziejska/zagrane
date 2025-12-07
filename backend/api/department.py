ad19012523755aa41aa11d9c58055d42a22273b0
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2025-12-07 05:35:28 +0100

    get department dates

--- backend/api/department.py
+++ backend/api/department.py
@@ -19,3 +19,25 @@ async def get_all_department_names(db: AsyncSession = Depends(get_db)):
     departments = result.scalars().all()
 
     return set([dep.type for dep in departments])
+
+@router.get("/get_department_dates")
+async def get_department_dates(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(DepartmentTables))
+    dept_tables = result.scalars().all()
+
+    output = []
+
+    for dt in dept_tables:
+        dep_result = await db.execute(
+            select(Departments).where(Departments.id == dt.department_id)
+        )
+        department = dep_result.scalars().first()
+
+        output.append([
+            department.type if department else None,
+            dt.end,
+            dt.start
+        ])
+
+    return output
+
